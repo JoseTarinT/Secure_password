8e8234a834218aa417dfa3f9bd1510f17336f06b
José Tarín <77952401+JoseTarinT@users.noreply.github.com>
2021-02-21 18:01:48 +1100

    Update PasswordGenerator.py
    On work

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -3,8 +3,25 @@ import string
 
 # We ask the users how many characters they want in the password
 lenght = int(input("How many characters do you want in your password?: "))
+combinations =  ["A: Just letters", "B: Just numbers/digits", "C: Numbers and letters", "D: Letters and special characters", "E: Numbers and special characters", "F: Letters, numbers and special characters"]
+print("What type of password would you prefer?")
+
+for choices in combinations:
+    print(choices)
+    
+passwordtype = str.lower(input())
+
 characters = string.ascii_letters + string.digits + string.punctuation
-# print(characters)
+a = string.ascii_letters
+b = string.digits
+c = a + b
+d = a
+
+password = "".join(secrets.choice(passwordtype) for x in range(lenght))
+
+if passwordtype == a:
+    print("Your password is: ", password)
+characters = string.ascii_letters + string.digits + string.punctuation
 
 password = "".join(secrets.choice(characters) for x in range(lenght))
 
