3d30a535314c3d020a3751b465db295ff84dbb7b
José Tarín <77952401+JoseTarinT@users.noreply.github.com>
2021-02-22 17:53:13 +1100

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -4,25 +4,38 @@ import string
 # We ask the users how many characters they want in the password
 lenght = int(input("How many characters do you want in your password?: "))
 combinations =  ["A: Just letters", "B: Just numbers/digits", "C: Numbers and letters", "D: Letters and special characters", "E: Numbers and special characters", "F: Letters, numbers and special characters"]
+
 print("What type of password would you prefer?")
 
 for choices in combinations:
     print(choices)
     
-passwordtype = str.lower(input())
-
-characters = string.ascii_letters + string.digits + string.punctuation
 a = string.ascii_letters
 b = string.digits
 c = a + b
-d = a
+d = a + string.punctuation
+e = b + string.punctuation
+f = string.ascii_letters + string.digits + string.punctuation
+    
+passwordtype = str.upper(input("Make a choice between A-F: "))
 
-password = "".join(secrets.choice(passwordtype) for x in range(lenght))
-
-if passwordtype == a:
-    print("Your password is: ", password)
-characters = string.ascii_letters + string.digits + string.punctuation
-
-password = "".join(secrets.choice(characters) for x in range(lenght))
-
-print("Your password is: ", password)
+if passwordtype == "A":
+    passworda = "".join(secrets.choice(a) for x in range(lenght))
+    print("Your password is: ", passworda)
+elif passwordtype == "B":
+    passwordb = "".join(secrets.choice(b) for x in range(lenght))
+    print("Your password is: ", passwordb)
+elif passwordtype == "C":
+    passwordc = "".join(secrets.choice(c) for x in range(lenght))
+    print("Your password is: ", passwordc)
+elif passwordtype == "D":
+    passwordd = "".join(secrets.choice(d) for x in range(lenght))
+    print("Your password is: ", passwordd)
+elif passwordtype == "E":
+    passworde = "".join(secrets.choice(e) for x in range(lenght))
+    print("Your password is: ", passworde)
+elif passwordtype == "F":
+    passwordf = "".join(secrets.choice(f) for x in range(lenght))
+    print("Your password is: ", passwordf)
+else:
+    print("Please, type a letter between A-F")
